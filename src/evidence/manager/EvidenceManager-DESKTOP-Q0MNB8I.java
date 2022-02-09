958270bc1fce6e78c6964ffe8ebca7029c93f00f
alomia <alomia18@gmail.com>
2022-02-09 00:40:49 -0500

    se completa funcionalidad del modulo de registro

---
+++ src/evidence/manager/EvidenceManager-DESKTOP-Q0MNB8I.java
@@ -0,0 +1,22 @@
+
+package evidence.manager;
+
+/**
+ *
+ * @author ALOMIA
+ */
+public class EvidenceManager {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+          java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+               // new Login().setVisible(true);
+            }
+        });
+    }
+    
+}
---
+++ src/evidence/manager/LogicaRegistro.java
@@ -0,0 +1,25 @@
+package evidence.manager;
+
+import java.sql.Array;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ALOMIA
+ */
+public class LogicaRegistro {
+
+    private Registro registro;
+    private ArrayList<Registro> nombre;
+    //private String nombreUsuario;
+
+    public LogicaRegistro() {
+        this.nombre = new ArrayList<Registro>();
+        registro = new Registro();
+     
+    }
+private void guardarNombre(){
+    
+    //nombre.add(registro.obtenerNombre());
+}
+}
