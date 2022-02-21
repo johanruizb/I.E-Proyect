a692a1b31bf95c8e45fae2a5d2ff9c276289cf2f
alomia <alomia18@gmail.com>
2022-02-20 22:30:42 -0500

    corregir alineacion

--- src/evidencia/ManejadorEvidencia.java
+++ src/evidencia/ManejadorEvidencia.java
@@ -5,7 +5,7 @@
 package evidencia;
 
 import java.awt.Color;
-import java.awt.FlowLayout;
+import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
@@ -24,41 +24,39 @@ import javax.swing.border.Border;
 public class ManejadorEvidencia extends JPanel implements Serializable {
 
     //private JPanel contenedor;
-    private JLabel icono, infoEstado, carpetaIcono;
-    private JLabel fechaEvidencia, iden, reference, descipcion1;
-    private String idEvidencia;
+    private JLabel identificador, fechaEvidencia, descipcion1, estado, iconoEstado;
+    //private String idEvidencia;
     //private JLabel separador1 = new JLabel();
-    private String descripcion, fecha;
-    private String estado, numeroReferencia;
+    //private String descripcion, fecha;
+    //private String estado, numeroReferencia;
     private ImageIcon carpeta = new ImageIcon("src/imagenes2/folder.png");
     //private ImageIcon separa2 = new ImageIcon("src/imagenes2/separa50.png");//13, 71, 161//13, 71, 161
     //private Border borde = BorderFactory.createBevelBorder(0, (new Color(214,214,214)), new Color(60,63,65));
     private Border borde = BorderFactory.createLineBorder(new Color(204, 204, 204), 2, true);
 
     public ManejadorEvidencia(String referencia, String fecha, String descripcion, String estado) {
-//     
-        
         //this.idEvidencia = id;
-        this.numeroReferencia = referencia;
-        this.fecha = fecha;
-        this.descripcion = descripcion;
-        this.estado = estado;
-        this.carpetaIcono = new JLabel(" ID:", new ImageIcon("src/imagenes2/folder.png"), SwingConstants.CENTER);
+        //this.numeroReferencia = referencia;
+        //this.fecha = fecha;
+        //this.descripcion = descripcion;
+        //this.estado = estado;
+        //this.carpetaIcono = new JLabel(" ID:", new ImageIcon("src/imagenes2/folder.png"), SwingConstants.CENTER);
         //this.iden = new JLabel(id,SwingConstants.CENTER);
-        this.reference = new JLabel(referencia, SwingConstants.LEFT);
-        this.fechaEvidencia = new JLabel(fecha, SwingConstants.CENTER);
-        this.descipcion1 = new JLabel(descripcion, SwingConstants.RIGHT);
+        // this.reference = new JLabel(referencia, SwingConstants.LEFT);
+        this.identificador = new JLabel(referencia, carpeta, JLabel.LEFT);
+        this.fechaEvidencia = new JLabel(fecha, JLabel.LEFT);
+        this.estado = new JLabel(descripcion, JLabel.LEFT);
         //this.descrip = new JLabel(descriccion);
 
         if (estado.equals("EN-ESPERA")) {
-            this.icono = new JLabel(new ImageIcon("src/imagenes2/en-aprobacion.png"), SwingConstants.CENTER);
+            this.iconoEstado = new JLabel(new ImageIcon("src/imagenes2/en-aprobacion.png"), SwingConstants.CENTER);
         } else if (estado.equals("APROBADO")) {
             // System.out.println("validando aprobado");
-            this.icono = new JLabel(new ImageIcon("src/imagenes2/aprobada.png"), SwingConstants.CENTER);
+            this.iconoEstado = new JLabel(new ImageIcon("src/imagenes2/aprobada.png"), SwingConstants.CENTER);
         } else if (estado.equals("RECHAZADA")) {
-            this.icono = new JLabel(new ImageIcon("src/imagenes2/rechazada.png"), SwingConstants.CENTER);
+            this.iconoEstado = new JLabel(new ImageIcon("src/imagenes2/rechazada.png"), SwingConstants.CENTER);
         } else {
-            this.icono = new JLabel(new ImageIcon("imagen/default.png"), SwingConstants.CENTER);
+            this.iconoEstado = new JLabel(new ImageIcon("imagen/default.png"), SwingConstants.CENTER);
             System.out.println("validando aprobado");
         }
         //JLabel n = new JLabel(separa);
@@ -66,22 +64,47 @@ public class ManejadorEvidencia extends JPanel implements Serializable {
         // iden.setHorizontalAlignment(SwingConstants.CENTER);
         this.setBorder(borde);
         this.setBackground(new Color(238, 238, 238));
-        this.setLayout(new GridLayout(1,5));
-       // this.setLayout(new GridBagLayout());
+        this.setLayout(new GridBagLayout());
+        // this.setLayout(new GridBagLayout());
         //GridBagConstraints c = new GridBagConstraints();
-       
+        GridBagConstraints limites = new GridBagConstraints();
+        //this.setPreferredSize(new Dimension(500, 50));
+        //carpetaIcono.setSize(30, 30);
 
-        this.setSize(500, 25);
-        carpetaIcono.setSize(30, 30);
-        this.add(carpetaIcono);
-    
-        this.add(this.reference);
+        JPanel state = new JPanel(new GridBagLayout());
+        //state.setPreferredSize(new Dimension(120, 40));
+        //this.setAlignmentX(LEFT_ALIGNMENT);
+        //limites.anchor = GridBagConstraints.CENTER;
+        limites.fill = GridBagConstraints.NONE;
 
-        this.add(this.fechaEvidencia);
-      
-        this.add(descipcion1);
+        limites.gridx = 0;
+        limites.gridy = 0;
+
+        int separacion = 290;
         
-        this.add(this.icono);
+        this.identificador.setPreferredSize(new Dimension(separacion, 40));
+        this.add(this.identificador, limites);
+
+        limites.gridx = 1;
+
+        this.fechaEvidencia.setPreferredSize(new Dimension(separacion, 40));
+        this.add(this.fechaEvidencia, limites);
+
+        limites.gridx = 0;
+
+        this.estado.setPreferredSize(new Dimension(70, 40));
+        state.add(this.estado, limites);
+        limites.gridx = 1;
+        state.add(this.iconoEstado, limites);
+
+        /*
+        limites.gridx = 1;
+        this.add(createEmptySpace(), limites);
+        limites.gridx = 3;
+        this.add(createEmptySpace(), limites);
+         */
+        limites.gridx = 3;
+        this.add(state, limites);
 
         this.repaint();
         this.revalidate();
@@ -90,68 +113,32 @@ public class ManejadorEvidencia extends JPanel implements Serializable {
 
     public ManejadorEvidencia(String stado) {
         this.estado = estado;
-       // this.numeroReferencia = referencia;
-       // this.fecha = fecha;
-       // this.descripcion = descripcion;
-       // this.estado = estado;
+        // this.numeroReferencia = referencia;
+        // this.fecha = fecha;
+        // this.descripcion = descripcion;
+        // this.estado = estado;
     }
 
-    ;
-
-    public String getFecha() {
-        return fecha;
+    public String getIdentificador() {
+        return identificador.getText();
     }
 
-    public String getNumerReferencia() {
-        return numeroReferencia;
+    public String getFechaEvidencia() {
+        return fechaEvidencia.getText();
     }
 
-    public JLabel getIcono() {
-        return icono;
-    }
-
-    public String getIdEvidencia() {
-        return idEvidencia;
-    }
-
-    public void setIdEvidencia(String idEvidencia) {
-        this.idEvidencia = idEvidencia;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
+    public String getDescipcion1() {
+        return descipcion1.getText();
     }
 
     public String getEstado() {
-        return estado;
+        return estado.getText();
     }
 
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
-
-    public void setIcono(JLabel icono) {
-        this.icono = icono;
-    }
-
-    public JLabel getId() {
-        return iden;
-    }
-
-    public void setId(JLabel id) {
-        this.iden = id;
-    }
-
-    public Border getBorde() {
-        return borde;
-    }
-
-    public void setBorde(Border borde) {
-        this.borde = borde;
+    private JLabel createEmptySpace() {
+        JLabel x = (new JLabel(""));
+        x.setBorder(BorderFactory.createEmptyBorder(0, 200, 0, 0));
+        return x;
     }
 
 }
