4db741458b622dbfce31091b20776ac5949a0995
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2022-02-22 21:31:56 -0500

    actualizar db

--- src/db/db.sql
+++ src/db/db.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS informacion_Personal(
 );
 
 CREATE TABLE IF NOT EXISTS informacion_Acceso(
-	serialInfo serial,
+	serial_info serial,
 	numeroCedula_FK VARCHAR(255) NOT NULL,
 	email VARCHAR(255) NOT NULL PRIMARY KEY,
 	contraseña VARCHAR(255) NOT NULL,
@@ -15,7 +15,7 @@ CREATE TABLE IF NOT EXISTS informacion_Acceso(
 );
 
 CREATE TABLE IF NOT EXISTS informacion_Locacion(
-	serialInfo serial,
+	serial_info serial,
 	numeroCedula_FK VARCHAR(255) NOT NULL PRIMARY KEY,
 	ciudad VARCHAR(255) NOT NULL,
 	empresa VARCHAR(255) NOT NULL,
@@ -23,7 +23,38 @@ CREATE TABLE IF NOT EXISTS informacion_Locacion(
 	FOREIGN KEY (numeroCedula_FK) REFERENCES informacion_Personal(numeroCedula) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-INSERT INTO informacion_Personal(numeroCedula,nombre,apellido1,apellido2) VALUES('1000000','Juan','Perez','NONE');
-INSERT INTO informacion_Acceso(numeroCedula_FK, email, contraseña) VALUES ('1000000','juan@gmail.com','juan123');
-INSERT INTO informacion_Locacion(numeroCedula_FK, ciudad, empresa, localidad) VALUES ('1000000','Cali','Inspecta','El Troncal');
+CREATE TABLE IF NOT EXISTS dummy(
+	serial_info serial,
+	cedula VARCHAR(255) NOT NULL PRIMARY KEY,
+);
 
+CREATE TABLE IF NOT EXISTS registro_evidencias(
+	serial_registro SERIAL,
+	id_evidencia VARCHAR(255) NOT NULL PRIMARY KEY,
+	id_informe VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS registro_detalles(
+	serial_registro SERIAL,
+	id_evidencia_FK VARCHAR(255) NOT NULL PRIMARY KEY,
+	registro_aprobado BOOLEAN NOT NULL,
+	fecha VARCHAR(255) NOT NULL,
+	ciudad VARCHAR(255) NOT NULL,
+	fecha_registro VARCHAR(255) NOT NULL,
+	FOREIGN KEY (id_evidencia_FK) REFERENCES registro_evidencias(id_evidencia) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+INSERT INTO
+	informacion_Personal(numeroCedula, nombre, apellido1, apellido2)
+VALUES
+	('1000000', 'Juan', 'Perez', 'NONE');
+
+INSERT INTO
+	informacion_Acceso(numeroCedula_FK, email, contraseña)
+VALUES
+	('1000000', 'juan@gmail.com', 'juan123');
+
+INSERT INTO
+	informacion_Locacion(numeroCedula_FK, ciudad, empresa, localidad)
+VALUES
+	('1000000', 'Cali', 'Inspecta', 'El Troncal');
