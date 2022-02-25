87a1740b29ebff51d2f49c415d65db0f9ecf25df
johanruizb <62685623+johanruizb@users.noreply.github.com>
2022-02-24 21:07:40 -0500

    remover forms

---
+++ src/login/Usuario.java
@@ -0,0 +1,89 @@
+package login;
+
+/**
+ *
+ * @author johan
+ */
+public class Usuario {
+
+    private String nombreUsuario = "", apellidosUsuario = "", numeroCedula = "";
+    private String emailUsuario = "", password = "";
+    private String ciudadUsuario = "", empresaUsuario = "", localidadUsuario = "";
+
+    public void setInformacionPersonal(String documento, String nombre, String apellidos) {
+        if ("".equals(nombreUsuario + apellidosUsuario + numeroCedula)) {
+            nombreUsuario = nombre;
+            apellidosUsuario = apellidos;
+            numeroCedula = documento;
+        }
+    }
+
+    public void setInformacionAcceso(String email, String contraseña) {
+        if ("".equals(emailUsuario + password)) {
+            emailUsuario = email;
+            password = contraseña;
+        }
+    }
+
+    public void setInformacionLocacion(String ciudad, String empresa, String localidad) {
+        if ("".equals(ciudadUsuario + empresaUsuario + localidadUsuario)) {
+            ciudadUsuario = ciudad;
+            empresaUsuario = empresa;
+            localidadUsuario = localidad;
+        }
+    }
+
+    /**
+     * getInformacionPersonal. Metodo que retorna la informacion personal del
+     * usuario.
+     *
+     * @return String[]. {numeroCedula, nombreUsuario, apellido1, apellido2}
+     * @author johan
+     */
+    public String[] getInformacionPersonal() {
+        if (!"".equals(nombreUsuario + apellidosUsuario + numeroCedula)) {
+
+            boolean lastname = apellidosUsuario.split(" ").length > 1;
+            boolean lastnameLength = false;
+            if (lastname) {
+                lastnameLength = (apellidosUsuario.split(" ")[1]).length() > 1;
+            }
+
+            String apellido1 = apellidosUsuario.split(" ")[0];
+            String apellido2 = lastname && lastnameLength ? (apellidosUsuario.split(" ")[1]) : "NONE";
+
+            return new String[]{numeroCedula, nombreUsuario, apellido1, apellido2};
+        }
+
+        return new String[]{null};
+    }
+
+    /**
+     * getInformacionAcceso. Metodo que retorna la informacion de acceso del
+     * usuario al sistema.
+     *
+     * @return String[]. {emailUsuario, password}
+     * @author johan
+     */
+    public String[] getInformacionAcceso() {
+        if (!"".equals(emailUsuario + password)) {
+            return new String[]{emailUsuario, password};
+        }
+        return new String[]{null};
+    }
+
+    /**
+     * getInformacionLocacion. Metodo que retorna la informacion de la locacion
+     * del usuario.
+     *
+     * @return String[]. {ciudadUsuario, empresaUsuario, localidadUsuario}
+     * @author johan
+     */
+    public String[] getInformacionLocacion() {
+        if (!"".equals(ciudadUsuario + empresaUsuario + localidadUsuario)) {
+            return new String[]{ciudadUsuario, empresaUsuario, localidadUsuario};
+        }
+        return new String[]{null};
+    }
+
+}
